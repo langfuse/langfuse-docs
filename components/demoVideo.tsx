6129c198893c46e7afa801a56f0fb51da8b64e4a
Marc Klingen <git@marcklingen.com>
2023-06-01 01:02:17 +0200

    ui

--- components/demoVideo.tsx
+++ components/demoVideo.tsx
@@ -4,8 +4,12 @@ export default function DemoVideo() {
   const cloudflareVideoId = "babadb14f745612e7bd428f53c8cc1c6";
 
   return (
-    <div className="mt-1 mb-4 overflow-hidden rounded-lg shadow-lg aspect-[1.6] bg-slate-50">
-      <Stream controls src={cloudflareVideoId} />
+    <div className="mt-1 mb-4 overflow-hidden rounded-lg shadow-lg aspect-[1.6] bg-slate-50 bg-cover bg-[url('/demo_thumbnail.jpg')]">
+      <Stream
+        controls
+        src={cloudflareVideoId}
+        poster="https://langfuse.com/demo_thumbnail.jpg"
+      />
     </div>
   );
 }
