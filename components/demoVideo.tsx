0e1cf55812edc0a17b092954f2cae2c30bc0586a
Marc Klingen <git@marcklingen.com>
2023-06-01 18:31:15 +0200

    Update demo

--- components/demoVideo.tsx
+++ components/demoVideo.tsx
@@ -1,10 +1,10 @@
 import { Stream } from "@cloudflare/stream-react";
 
 export default function DemoVideo() {
-  const cloudflareVideoId = "babadb14f745612e7bd428f53c8cc1c6";
+  const cloudflareVideoId = "dd8e215b11a494d2ce4409e4a95b52df";
 
   return (
-    <div className="mt-1 mb-4 overflow-hidden rounded-lg shadow-lg aspect-[1.6] bg-slate-50 bg-cover bg-[url('/demo_thumbnail.jpg')]">
+    <div className="mt-1 mb-4 overflow-hidden rounded-lg shadow-lg aspect-[1.55] bg-slate-50 bg-cover bg-[url('/demo_thumbnail.jpg')]">
       <Stream
         controls
         src={cloudflareVideoId}
