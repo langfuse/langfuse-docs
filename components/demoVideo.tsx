969d69ce3201b89a9009d88278cb2c08c2ae521e
Marc Klingen <git@marcklingen.com>
2023-06-01 18:52:02 +0200

    Misc

--- components/demoVideo.tsx
+++ components/demoVideo.tsx
@@ -4,7 +4,7 @@ export default function DemoVideo() {
   const cloudflareVideoId = "dd8e215b11a494d2ce4409e4a95b52df";
 
   return (
-    <div className="mt-1 mb-4 overflow-hidden rounded-lg shadow-lg aspect-[1.55] bg-slate-50 bg-cover bg-[url('/demo_thumbnail.jpg')]">
+    <div className="mt-3 mb-6 overflow-hidden rounded-xl shadow-lg aspect-[1.55] ring-1 ring-slate-700 bg-slate-50 bg-cover bg-[url('/demo_thumbnail.jpg')]">
       <Stream
         controls
         src={cloudflareVideoId}
