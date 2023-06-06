7ed69ca77ab195ab7084659b3598b7cb72cca25e
Marc Klingen <git@marcklingen.com>
2023-06-06 21:22:41 +0200

    wip

--- components/cards.tsx
+++ components/cards.tsx
@@ -1,7 +1,25 @@
-export function TwoCards({ children }) {
-  return <div className="grid grid-cols-2 gap-3 my-24">{children}</div>;
+import cn from "clsx";
+
+export function TwoCards({ children, className }) {
+  return (
+    <div className={cn("grid md:grid-cols-2 gap-7", className)}>{children}</div>
+  );
 }
 
-export function StartCard({ children, isDark }) {
-  return <div className="rounded-md ring-1 ring-gray-500 px-5">{children}</div>;
+export function StartCard({ children, isDark, isOutline }) {
+  return (
+    <div
+      className={cn(
+        "rounded-md ring-1 ring-gray-200 dark:ring-gray-800 px-5 pb-5 bg-[conic-gradient(at_bottom_right,_var(--tw-gradient-stops))]",
+        isOutline
+          ? "bg-transparent from-transparent via-transparent to-transparent ring-2"
+          : null,
+        isDark
+          ? "[&>*]:text-white from-slate-300 via-slate-700 to-slate-400 dark:from-slate-800 dark:via-slate-900 dark:to-slate-800"
+          : "from-indigo-50 via-slate-300 to-indigo-50 dark:from-slate-900 dark:via-black dark:to-slate-900"
+      )}
+    >
+      {children}
+    </div>
+  );
 }
