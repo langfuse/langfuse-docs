d5e0f043fb2282489360f2f6d29f80387e1118a2
Marc Klingen <git@marcklingen.com>
2023-06-07 17:01:29 +0200

    Screen sizes

--- components/feature.tsx
+++ components/feature.tsx
@@ -7,24 +7,24 @@ export function Feature(props: {
   children;
 }) {
   return (
-    <div className="my-16 md:my-32 md:grid md:grid-cols-5 md:gap-10 xl:gap-20 md:items-center">
+    <div className="my-16 md:my-32 lg:grid lg:grid-cols-3 md:gap-10 md:items-center">
       {props.imagePosition === "right" ? (
-        <div className="mb-8 md:mb-0 md:col-span-2 md:text-center [&>h2]:mt-0">
+        <div className="mb-8 lg:mb-0 md:text-center [&>h2]:mt-0">
           {props.children}
         </div>
       ) : (
-        <div className="mb-8 md:hidden">{props.children}</div>
+        <div className="mb-8 md:text-center lg:hidden">{props.children}</div>
       )}
 
       <Image
         alt={props.imageAlt}
         src={props.src}
         style={{ objectFit: "contain" }}
-        className="col-span-3 max-h-96"
+        className="lg:col-span-2 max-h-96"
       />
 
       {props.imagePosition === "left" ? (
-        <div className="hidden mb-8 md:mb-0 md:block md:col-span-2 md:text-center [&>h2]:mt-0">
+        <div className="hidden mb-8 lg:mb-0 lg:block md:text-center [&>h2]:mt-0">
           {props.children}
         </div>
       ) : null}
