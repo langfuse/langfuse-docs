ad3aaba8888e76e42d36b5693a353a0d3c6f86f3
Marc Klingen <git@marcklingen.com>
2023-06-07 14:20:51 +0200

    Fix lightbox

--- components/demoVideo.tsx
+++ components/demoVideo.tsx
@@ -1,15 +1,14 @@
 import { Stream } from "@cloudflare/stream-react";
-import { useState, Fragment } from 'react'
-import { Dialog, Transition } from '@headlessui/react'
+import { useState, Fragment } from "react";
+import { Dialog, Transition } from "@headlessui/react";
 import { XSquareIcon } from "lucide-react";
 import { Card } from "nextra-theme-docs";
 import { Clapperboard } from "lucide-react";
 import { SiGithub } from "react-icons/si";
 import { AiOutlineCloud } from "react-icons/ai";
 
- function VideoPlayer() {
+export function VideoPlayer() {
   const cloudflareVideoId = "dd8e215b11a494d2ce4409e4a95b52df";
-
   return (
     <div className="mt-3 mb-6 overflow-hidden rounded-xl shadow-lg aspect-[1.55] ring-1 ring-slate-700 bg-slate-50 bg-cover bg-[url('/demo_thumbnail.jpg')]">
       <Stream
@@ -21,8 +20,8 @@ import { AiOutlineCloud } from "react-icons/ai";
   );
 }
 
-export default function Buttons() {
-  const [open, setOpen] = useState<boolean>(false)
+export function Buttons() {
+  const [open, setOpen] = useState<boolean>(false);
   return (
     <>
       <div onClick={() => setOpen(true)}>
@@ -33,7 +32,12 @@ export default function Buttons() {
           href="#"
         />
       </div>
-      <Card icon={<span>🚀</span>} title="Quickstart" href="/docs/get-started" children={null}/>
+      <Card
+        icon={<span>🚀</span>}
+        title="Quickstart"
+        href="/docs/get-started"
+        children={null}
+      />
       <Card
         icon={<SiGithub size="24" />}
         title="Repo"
@@ -47,10 +51,9 @@ export default function Buttons() {
         children={null}
       />
       <DemoVideo open={open} setOpen={setOpen} />
-  </>
-  )
+    </>
+  );
 }
-
 
 interface DemoVideoProps {
   open: boolean;
@@ -58,11 +61,14 @@ interface DemoVideoProps {
 }
 
 function DemoVideo(props: DemoVideoProps) {
-
   return (
     <>
       <Transition.Root show={props.open} as={Fragment}>
-        <Dialog as="div" className="relative z-10 w-screen" onClose={props.setOpen}>
+        <Dialog
+          as="div"
+          className="relative z-10 w-screen"
+          onClose={props.setOpen}
+        >
           <Transition.Child
             as={Fragment}
             enter="ease-out duration-300"
@@ -86,7 +92,7 @@ function DemoVideo(props: DemoVideoProps) {
                 leaveFrom="opacity-100 translate-y-0 sm:scale-100"
                 leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
               >
-                <Dialog.Panel className="relative transform overflow-hidden px-4 pb-4 pt-5 transition-all sm:my-8 w-full md:w-2/3 mx-auto sm:p-6">
+                <Dialog.Panel className="relative transform overflow-hidden px-4 pb-4 pt-5 transition-all sm:my-8 w-full lg:w-2/3 mx-auto sm:p-6">
                   <div className="absolute right-0 top-0 hidden pr-4 pt-4 sm:block">
                     <button
                       type="button"
@@ -94,13 +100,16 @@ function DemoVideo(props: DemoVideoProps) {
                       onClick={() => props.setOpen(false)}
                     >
                       <span className="sr-only">Close</span>
-                      <XSquareIcon className="h-6 w-6 bg-transparent" aria-hidden="true" />
+                      <XSquareIcon
+                        className="h-6 w-6 bg-transparent"
+                        aria-hidden="true"
+                      />
                     </button>
                   </div>
-                    <div className="text-center sm:ml-4 sm:mt-0 sm:text-left pt-5">
-                      <div className="mt-2">
-                        <VideoPlayer />
-                      </div>
+                  <div className="text-center sm:ml-4 sm:mt-0 sm:text-left pt-5">
+                    <div className="mt-2">
+                      <VideoPlayer />
+                    </div>
                   </div>
                 </Dialog.Panel>
               </Transition.Child>
@@ -109,5 +118,5 @@ function DemoVideo(props: DemoVideoProps) {
         </Dialog>
       </Transition.Root>
     </>
-  )
+  );
 }
