1ea3b174f1c260f4b88101493e577517c9c9420a
Marc Klingen <git@marcklingen.com>
2023-06-07 15:08:14 +0200

    push

--- components/cards.tsx
+++ components/cards.tsx
@@ -10,7 +10,7 @@ export function StartCard({ children, isDark, isOutline }) {
   return (
     <div
       className={cn(
-        "flex flex-col rounded-md ring-1 ring-gray-200 dark:ring-gray-800 px-5 pb-5 bg-[conic-gradient(at_bottom_right,_var(--tw-gradient-stops))]",
+        "flex flex-col rounded-md ring-1 [&>h2]:mt-0 ring-gray-200 dark:ring-gray-800 p-5 bg-[conic-gradient(at_bottom_right,_var(--tw-gradient-stops))]",
         isOutline
           ? "bg-transparent from-transparent via-transparent to-transparent ring-2"
           : "from-slate-100 via-slate-400 to-slate-200 dark:from-slate-800 dark:via-slate-900 dark:to-slate-800"
--- components/feature.tsx
+++ components/feature.tsx
@@ -7,22 +7,26 @@ export function Feature(props: {
   children;
 }) {
   return (
-    <div className="md:grid md:grid-cols-5 md:gap-7 my-16">
+    <div className="my-16 md:my-32 md:grid md:grid-cols-5 md:gap-7 md:items-center">
       {props.imagePosition === "right" ? (
-        <div className="md:col-span-2">{props.children}</div>
+        <div className="mb-8 md:mb-0 md:col-span-2 md:text-center [&>h2]:mt-0">
+          {props.children}
+        </div>
       ) : (
-        <div className="md:hidden">{props.children}</div>
+        <div className="mb-8 md:hidden">{props.children}</div>
       )}
 
       <Image
         alt={props.imageAlt}
         src={props.src}
         style={{ objectFit: "contain" }}
-        className="col-span-3"
+        className="col-span-3 max-h-96"
       />
 
       {props.imagePosition === "left" ? (
-        <div className="hidden md:block md:col-span-2">{props.children}</div>
+        <div className="hidden mb-8 md:mb-0 md:block md:col-span-2 md:text-center [&>h2]:mt-0">
+          {props.children}
+        </div>
       ) : null}
     </div>
   );
