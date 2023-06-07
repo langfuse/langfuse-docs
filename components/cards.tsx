7440596a2c85d39da83c522491b2e912891c2123
Marc Klingen <git@marcklingen.com>
2023-06-07 15:31:05 +0200

    Add divider

--- components/cards.tsx
+++ components/cards.tsx
@@ -1,8 +1,22 @@
 import cn from "clsx";
 
 export function TwoCards({ children, className }) {
+  return <div className={cn("md:flex gap-5", className)}>{children}</div>;
+}
+
+export function Divider({ text }) {
   return (
-    <div className={cn("grid md:grid-cols-2 gap-7", className)}>{children}</div>
+    <div className="relative">
+      <div
+        className="absolute inset-0 flex flex-col items-center"
+        aria-hidden="true"
+      >
+        <div className="h-full border-l border-gray-300" />
+      </div>
+      <div className="relative flex flex-col h-full justify-center">
+        <div className="bg-white py-2 text-sm text-gray-500">{text}</div>
+      </div>
+    </div>
   );
 }
 
@@ -10,7 +24,7 @@ export function StartCard({ children, isDark, isOutline }) {
   return (
     <div
       className={cn(
-        "flex flex-col rounded-md ring-1 [&>h2]:mt-0 [&>h3]:mt-0 [&>h4]:mt-0 ring-gray-200 dark:ring-gray-800 p-5 bg-[conic-gradient(at_bottom_right,_var(--tw-gradient-stops))]",
+        "flex-1 flex-col rounded-md ring-1 [&>h2]:mt-0 [&>h3]:mt-0 [&>h4]:mt-0 ring-gray-200 dark:ring-gray-800 p-5 bg-[conic-gradient(at_bottom_right,_var(--tw-gradient-stops))]",
         isOutline
           ? "bg-transparent from-transparent via-transparent to-transparent ring-2"
           : "from-slate-100 via-slate-400 to-slate-200 dark:from-slate-800 dark:via-slate-900 dark:to-slate-800"
