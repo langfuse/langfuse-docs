d7668859de53d984c374b5321f3457b5a22b0d6e
Marc Klingen <git@marcklingen.com>
2023-06-23 01:58:18 +0200

    Add email signup

---
+++ pages/api/productUpdateSignup.ts
@@ -0,0 +1,40 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    const [slackResponse, loopsResponse] = await Promise.all([
+      fetch(process.env.SLACK_WEBHOOK_URL, {
+        method: "POST",
+        body: JSON.stringify({ rawMessage: JSON.stringify(req.body, null, 2) }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }),
+      fetch("https://app.loops.so/api/v1/contacts/create", {
+        method: "POST",
+        body: JSON.stringify({
+          email: req.body.email,
+          source: "Website signup",
+        }),
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.LOOPS_API_KEY}`,
+        },
+      }),
+    ]);
+
+    if (slackResponse.status === 200 && loopsResponse.status === 200) {
+      res.status(200).json({ status: "OK" });
+    } else {
+      console.error(slackResponse);
+      console.error(loopsResponse);
+      res.status(400).json({ status: "Error" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ status: "Error" });
+  }
+}
