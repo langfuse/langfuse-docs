52c3fe143056558f1f9613677ef85304eb5e7c65
Marc Klingen <git@marcklingen.com>
2023-07-26 12:13:58 +0200

    fix sitemap to include / (#16)

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,4 +2,11 @@
 module.exports = {
     siteUrl: 'https://langfuse.com',
     generateRobotsTxt: true,
+    changefreq: 'daily',
+    additionalPaths: async (config) => [{
+        loc: '/',
+        priority: 1,
+        changefreq: 'daily',
+        lastmod: new Date().toISOString()
+    }]
 }
