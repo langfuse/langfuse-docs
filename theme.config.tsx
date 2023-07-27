949747beeeed7f25c8ba0ba48ac217fc2dd19f62
Marc Klingen <git@marcklingen.com>
2023-07-27 15:40:39 +0200

    menu

--- theme.config.tsx
+++ theme.config.tsx
@@ -29,6 +29,10 @@ const config: DocsThemeConfig = {
   chat: {
     link: "https://discord.gg/7NXusRtqYU",
   },
+  sidebar: {
+    autoCollapse: true,
+    defaultMenuCollapseLevel: 1,
+  },
   docsRepositoryBase: "https://github.com/langfuse/langfuse-docs/tree/main",
   footer: {
     text: (
