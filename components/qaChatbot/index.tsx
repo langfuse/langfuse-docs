89d58c692040ff3b4c75c668b03f55ee545f1c03
Marc Klingen <git@marcklingen.com>
2023-07-31 20:48:29 +0200

    add classname support

--- components/qaChatbot/index.tsx
+++ components/qaChatbot/index.tsx
@@ -9,8 +9,9 @@ import React from "react";
 import { Send } from "lucide-react";
 import { nanoid } from "ai";
 import { useMemo, useRef, useState } from "react";
+import { cn } from "@/lib/utils";
 
-export function Chat() {
+export function Chat({ className }: { className?: string }) {
   const ref = React.useRef<HTMLDivElement>(null);
   const conversationId = useMemo(() => nanoid(), []);
 
@@ -55,7 +56,12 @@ export function Chat() {
   const messagesWithWelcome: Message[] = [welcomeMessage, ...messages];
 
   return (
-    <div className="relative flex flex-col p-5 mt-10 h-[70vh] overflow-hidden border border-gray-700 rounded-lg">
+    <div
+      className={cn(
+        "relative flex flex-col p-5 mt-10 h-[70vh] overflow-hidden border border-gray-700 rounded-lg",
+        className
+      )}
+    >
       <div className="flex-1 overflow-y-scroll" ref={ref}>
         <ChatList
           messages={messagesWithWelcome}
@@ -99,16 +105,3 @@ const welcomeMessage: Message = {
 
 _⚠️ Warning: The bot is not very smart and answers might be misleading. If you have a question that the bot cannot answer, ask the founders via the chat widget._`,
 } as const;
-
-// **Uses:**
-
-// - [supabase/headless-vector-search](https://github.com/supabase/headless-vector-search) to embed the docs via GitHub Actions
-// - Supabase with pgvector
-// - [Vercel AI SDK](https://github.com/vercel-labs/ai)
-// - UI components from [ai-chatbot](https://github.com/vercel-labs/ai-chatbot)
-// - GPT-3.5
-// - Langfuse (of course)
-
-// **Code:**
-// - Frontend
-// - Backend API route
