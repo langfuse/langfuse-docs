3c24a5e697a1332148fa1ee4c361c3de0f4c09df
Marc Klingen <git@marcklingen.com>
2023-08-01 22:52:48 +0200

    update redirects

--- next.config.mjs
+++ next.config.mjs
@@ -43,19 +43,20 @@ export default withNextra({
 
 const nonPermanentRedirects = [
   ["/analytics", "https://docs.google.com/document/d/1PEFSqn-VWjNXOZZ1U7FC0oH-spDdkKJxLwgp15iK7zY"],
-  ["/observability", "/integrations"],
-  ["/integrations", "/docs/integrations"],
-  ["/docs/reference", "/docs/api"],
-  ["/docs/debugging-ui", "/docs/tracing"],
 ];
 
 const permanentRedirects = [
+  // Migration 2023-07
+  ["/observability", "/docs/tracing"],
+  ["/docs/debugging-ui", "/docs/tracing"],
   // Migration 2023-08-01
   // deployment
   ["/docs/local", "/docs/deployment/local"],
   ["/docs/self-host", "/docs/deployment/self-host"],
   ["/docs/cloud", "/docs/deployment/cloud"],
   // integrations
+  ["/integrations", "/docs/integrations"],
+  ["/docs/reference", "/docs/integrations/api"],
   ["/docs/api", "/docs/integrations/api"],
   ["/docs/sdk", "/docs/integrations/sdk"],
   ["/docs/langchain", "/docs/integrations/langchain"],
