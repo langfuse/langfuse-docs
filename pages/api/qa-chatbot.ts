6b37241f361d361e832856095cd5137e24aa1412
Marc Klingen <git@marcklingen.com>
2023-08-10 16:36:46 +0200

    upgrade to beta

--- pages/api/qa-chatbot.ts
+++ pages/api/qa-chatbot.ts
@@ -28,6 +28,7 @@ const langfuse = new Langfuse({
   publicKey: process.env.NEXT_PUBLIC_LANGFUSE_PUBLIC_KEY,
   secretKey: process.env.LANGFUSE_SECRET_KEY,
 });
+// langfuse.debug();
 
 export default async function handler(req: Request, res: Response) {
   const body = await req.json();
@@ -35,7 +36,7 @@ export default async function handler(req: Request, res: Response) {
 
   const trace = langfuse.trace({
     name: "qa",
-    externalId: body.conversationId,
+    id: "lf.docs.conversation." + body.conversationId,
   });
 
   const messages = body.messages;
