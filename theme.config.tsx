4c3bdca03d05ca45741ab7686a75d556cc33dab4
Marc Klingen <git@marcklingen.com>
2023-08-11 23:07:56 +0200

    Add X Link

--- theme.config.tsx
+++ theme.config.tsx
@@ -44,6 +44,27 @@ const config: DocsThemeConfig = {
   chat: {
     link: "https://discord.gg/7NXusRtqYU",
   },
+  navbar: {
+    extraContent: (
+      <a
+        className="p-2"
+        target="_blank"
+        href="https://x.com/langfuse"
+        aria-label="Langfuse X formerly known as Twitter"
+        rel="nofollow noreferrer"
+      >
+        <svg
+          aria-label="X formerly known as Twitter"
+          fill="currentColor"
+          width="24"
+          height="24"
+          viewBox="0 0 24 22"
+        >
+          <path d="M16.99 0H20.298L13.071 8.26L21.573 19.5H14.916L9.702 12.683L3.736 19.5H0.426L8.156 10.665L0 0H6.826L11.539 6.231L16.99 0ZM15.829 17.52H17.662L5.83 1.876H3.863L15.829 17.52Z"></path>
+        </svg>
+      </a>
+    ),
+  },
   docsRepositoryBase: "https://github.com/langfuse/langfuse-docs/tree/main",
   footer: {
     text: (
