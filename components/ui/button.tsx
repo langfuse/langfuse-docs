7ceb7f339317d7389052c338fda726695cb3a2d8
Marc Klingen <git@marcklingen.com>
2023-08-14 03:25:09 +0200

    UI

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -9,11 +9,11 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        default: "bg-primary/70 text-primary-foreground hover:bg-primary/80",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
-          "border border-input bg-background/30 hover:bg-accent hover:text-accent-foreground",
+          "border border-input bg-background/30 hover:bg-accent/70 hover:text-accent-foreground",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
