7cc7c328f7e7fff01f4525eed55006a1cff2828c
Marc Klingen <git@marcklingen.com>
2023-08-23 01:07:54 +0200

    Edge config

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,11 @@
 import { type NextApiRequest } from "next";
 import { NextRequest } from "next/server";
+import { get } from "@vercel/edge-config";
 
 export async function middleware(req: NextRequest) {
   const ip = getIP(req);
+  const config = await get("blockedIps");
+  const blockedIps = Array.isArray(config) ? config : [config];
 
   if (blockedIps.includes(ip)) {
     console.log("Blocked request by ip: ", ip);
@@ -20,5 +23,3 @@ export default function getIP(request: Request | NextApiRequest) {
 
   return xff ? (Array.isArray(xff) ? xff[0] : xff.split(",")[0]) : "127.0.0.1";
 }
-
-const blockedIps = ["49.207.204.122"];
