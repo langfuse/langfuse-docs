d889719d7b883cedd2b2cb09b860486d745679b9
Marc Klingen <git@marcklingen.com>
2023-09-07 18:46:33 +0200

    Aug Update

--- components/Frame.tsx
+++ components/Frame.tsx
@@ -3,16 +3,22 @@ import { cn } from "@/lib/utils";
 export const Frame = ({
   children,
   className,
+  border = false,
 }: {
   children: React.ReactNode;
   className?: string;
+  border?: boolean;
 }) => (
   <div
     className={cn(
-      "relative rounded-md sm:rounded-xl overflow-hidden max-w-2xl my-4 bg-primary/5",
+      "my-4",
+      border &&
+        "p-1 pb-0 bg-gradient-to-tr from-blue-300/50 via-green-200/50 to-yellow-300/50 inline-block rounded-lg sm:rounded-2xl",
       className
     )}
   >
-    {children}
+    <div className="inline-block rounded-md sm:rounded-xl overflow-hidden max-w-2xl bg-primary/5">
+      {children}
+    </div>
   </div>
 );
