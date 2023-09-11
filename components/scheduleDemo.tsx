076c8d6c5cc0a408179bd8d3eab70760efa19df2
Marc Klingen <git@marcklingen.com>
2023-09-12 00:43:39 +0200

    Add demo page in docs (fix pagination)

--- components/scheduleDemo.tsx
+++ components/scheduleDemo.tsx
@@ -2,6 +2,21 @@ import { Background } from "./Background";
 import Cal, { getCalApi } from "@calcom/embed-react";
 import { useEffect } from "react";
 
+export function ScheduleDemoPage() {
+  return (
+    <section className="flex flex-col gap-10 w-full min-h-screen items-center py-20">
+      <div className="text-center">
+        <h1>Schedule Demo</h1>
+        <p className="text-lg text-primary/80 mt-3">
+          Talk to the founders to learn more about Langfuse
+        </p>
+      </div>
+      <ScheduleDemo />
+      <Background />
+    </section>
+  );
+}
+
 export function ScheduleDemo() {
   useEffect(() => {
     (async function () {
@@ -13,21 +28,11 @@ export function ScheduleDemo() {
       });
     })();
   }, []);
-
   return (
-    <section className="flex flex-col gap-10 w-full min-h-screen items-center py-20">
-      <div className="text-center">
-        <h1>Schedule Demo</h1>
-        <p className="text-lg text-primary/80 mt-3">
-          Talk to the founders to learn more about Langfuse
-        </p>
-      </div>
-      <Cal
-        calLink="marc-kl/demo"
-        style={{ width: "100%", height: "100%", overflow: "scroll" }}
-        config={{ layout: "month_view" }}
-      />
-      <Background />
-    </section>
+    <Cal
+      calLink="marc-kl/demo"
+      style={{ width: "100%", height: "100%", overflow: "scroll" }}
+      config={{ layout: "month_view" }}
+    />
   );
 }
