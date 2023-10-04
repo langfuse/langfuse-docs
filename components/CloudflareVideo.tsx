6feca011431986cc3751cc5736cfee3ef140f513
Marc Klingen <git@marcklingen.com>
2023-10-03 19:15:54 -0700

    Update September (#97)

--- components/CloudflareVideo.tsx
+++ components/CloudflareVideo.tsx
@@ -5,15 +5,20 @@ export const CloudflareVideo = ({
   videoId,
   aspectRatio,
   className,
+  gifStyle = false,
 }: {
   videoId: string;
   aspectRatio?: number;
+  gifStyle: boolean;
   className?: string;
 }) => (
   <MediaPlayer
     src={`https://customer-xnej9vqjtgxpafyk.cloudflarestream.com/${videoId}/manifest/video.m3u8`}
     poster={`https://customer-xnej9vqjtgxpafyk.cloudflarestream.com/${videoId}/thumbnails/thumbnail.gif`}
-    controls
+    controls={!gifStyle}
+    autoplay={gifStyle}
+    muted={gifStyle}
+    loop={gifStyle}
     aspectRatio={aspectRatio ?? 16 / 9}
     className={cn(
       "my-4 overflow-hidden rounded-xl shadow-lg ring-1 ring-slate-700",
