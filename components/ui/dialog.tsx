1aee724eca4baed78638ab9ca4e3b296014a6b40
Marc Klingen <git@marcklingen.com>
2023-10-06 13:35:24 -0700

    simplify

--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -10,10 +10,7 @@ const Dialog = DialogPrimitive.Root;
 
 const DialogTrigger = DialogPrimitive.Trigger;
 
-const DialogPortal = (props: DialogPrimitive.DialogPortalProps) => (
-  <DialogPrimitive.Portal {...props} />
-);
-DialogPortal.displayName = DialogPrimitive.Portal.displayName;
+const DialogPortal = DialogPrimitive.Portal;
 
 const DialogOverlay = React.forwardRef<
   React.ElementRef<typeof DialogPrimitive.Overlay>,
