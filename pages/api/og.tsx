3b9a28cc1a1a5a3715b6dd5f8879cc032b1bcdfd
Marc Klingen <git@marcklingen.com>
2023-10-06 16:56:35 -0700

    OG image (#104)

---
+++ pages/api/og.tsx
@@ -0,0 +1,93 @@
+import { ImageResponse } from "@vercel/og";
+import { NextRequest } from "next/server";
+
+export const config = {
+  runtime: "edge",
+};
+
+export default async function handler(request: NextRequest) {
+  const imageData = (await fetch(
+    new URL("../../public/icon256.png", import.meta.url)
+  ).then((res) => res.arrayBuffer())) as string;
+
+  const { searchParams } = new URL(request.url);
+
+  // ?title=<title>
+  const rawTitle = searchParams.has("title")
+    ? searchParams.get("title")
+    : undefined;
+  const title = rawTitle ?? "Langfuse";
+
+  const rawDescription = searchParams.has("description")
+    ? searchParams.get("description")
+    : undefined;
+  const description = rawDescription
+    ? rawDescription.length > 150
+      ? rawDescription.slice(0, 150) + "..."
+      : rawDescription
+    : undefined;
+
+  const section = searchParams.has("section")
+    ? searchParams.get("section")
+    : undefined;
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          height: "100%",
+          width: "100%",
+          backgroundImage: "radial-gradient(at center top, #283557, #000000)",
+          color: "#fff",
+          padding: 0,
+          fontWeight: 500,
+          fontSize: 40,
+          fontFamily: "sans-serif",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            gap: 20,
+            alignItems: "center",
+            padding: 40,
+            borderBottom: "3px solid white",
+          }}
+        >
+          <img width="50" height="50" src={imageData} />
+          <span style={{ fontWeight: 800 }}>
+            Langfuse
+            <span style={{ marginLeft: 10, fontWeight: 400 }}>
+              – Open source observability for LLM Apps
+            </span>
+          </span>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flex: 1,
+            alignItems: "flex-start",
+            justifyContent: "center",
+            flexDirection: "column",
+            padding: 40,
+            gap: 10,
+          }}
+        >
+          {section ? (
+            <div style={{ fontWeight: 700, fontSize: 40, color: "#aaa" }}>
+              {section}
+            </div>
+          ) : null}
+          <div style={{ fontWeight: 700, fontSize: 80 }}>{title}</div>
+          {description ? <div>{description}</div> : null}
+        </div>
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 630,
+    }
+  );
+}
