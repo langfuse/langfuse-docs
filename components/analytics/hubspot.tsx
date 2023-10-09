e4283cd9b431c5e90d473d564f936cea8a843983
Marc Klingen <git@marcklingen.com>
2023-10-09 22:38:20 +0200

    Add hs analytics (#112)

---
+++ components/analytics/hubspot.tsx
@@ -0,0 +1,12 @@
+import Script from "next/script";
+
+export const Hubspot = () => {
+  return <Script src="https://js-eu1.hs-scripts.com/143255669.js" />;
+};
+
+export const hsPageView = (path: string) => {
+  if (window._hsq) {
+    console.log("available", window._hsq);
+    window._hsq.push(["setPath", path]);
+  }
+};
