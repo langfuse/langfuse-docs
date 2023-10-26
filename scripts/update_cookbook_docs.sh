2e33eba85e15688e5d73bce65dc55ca68f80ee6b
Marc Klingen <git@marcklingen.com>
2023-10-26 20:01:06 +0200

    Add RAGAS cookbook

--- scripts/update_cookbook_docs.sh
+++ scripts/update_cookbook_docs.sh
@@ -4,4 +4,5 @@ mv cookbook/datasets.md pages/docs/datasets/python-cookbook.md
 mv cookbook/integration_langchain.md pages/docs/integrations/langchain/python.md
 mv cookbook/integration_openai_sdk.md pages/docs/integrations/openai.md
 mv cookbook/evaluation_get_started.md pages/docs/scores/model-based-evals.md
+mv cookbook/evaluation_of_rag_with_ragas.md pages/docs/scores/model-based-evals/ragas.md
 mv cookbook/python_sdk.md pages/docs/integrations/sdk/python.md
