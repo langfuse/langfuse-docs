76c767d0aea670379325a9792fd1458dc21480b7
Marc Klingen <git@marcklingen.com>
2023-11-07 09:22:06 +0100

    Banner

--- theme.config.tsx
+++ theme.config.tsx
@@ -216,18 +216,18 @@ const config: DocsThemeConfig = {
     Card,
     Cards,
   },
-  // banner: {
-  //   key: "seed-banner",
-  //   dismissible: false,
-  //   text: (
-  //     <Link href="/blog/announcing-our-seed-round">
-  //       <span className="sm:hidden">Langfuse raised $4M →</span>
-  //       <span className="hidden sm:inline">
-  //         Langfuse raised $4M. Read the full announcement & what's next →
-  //       </span>
-  //     </Link>
-  //   ),
-  // },
+  banner: {
+    key: "seed-banner",
+    dismissible: false,
+    text: (
+      <Link href="/blog/announcing-our-seed-round">
+        <span className="sm:hidden">Langfuse raised $4M →</span>
+        <span className="hidden sm:inline">
+          Langfuse raised $4M. Read the full announcement & what's next →
+        </span>
+      </Link>
+    ),
+  },
 };
 
 export default config;
