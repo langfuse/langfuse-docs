4de30b683e0b7af479d5d89221a79b4bd03615ee
Marc Klingen <git@marcklingen.com>
2024-01-24 14:54:33 +0100

    docs: api limits

--- components/home/Pricing.tsx
+++ components/home/Pricing.tsx
@@ -244,6 +244,11 @@ const faqs = [
     answer:
       "Langfuse Cloud is hosted on AWS and data is stored in the US or EU depending on your selection. See our <a class='underline' href='/docs/data-security-privacy'>security and privacy documentation</a> for more details.",
   },
+  {
+    question: "What are the limitations of the fair use policy?",
+    answer:
+      "The fair use policy is designed to be generous. By default, we limit API usage at 1k requests per minute to prevent abuse. Since requests are batched via the Langfuse client SDKs and the /api/public/ingestion endpoint, this limit is unlikely to be reached by most customers. If you need to increase this limit, please contact us.",
+  },
 ];
 
 export function PricingFAQ() {
