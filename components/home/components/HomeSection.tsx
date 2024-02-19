b6fe3b786c81ddc9bda4477f90279d79cac885a9
Marc Klingen <git@marcklingen.com>
2024-02-19 15:25:34 +0100

    fix: padding

--- components/home/components/HomeSection.tsx
+++ components/home/components/HomeSection.tsx
@@ -9,7 +9,7 @@ export const HomeSection = forwardRef<
     <section
       ref={ref}
       className={cn(
-        "py-20 lg:py-32 mx-auto max-w-7xl px-5 sm:px-7 xl:px-10 first:pt-10 last:pb-36",
+        "py-20 lg:py-32 mx-auto max-w-7xl px-5 sm:px-7 xl:px-10 first:pt-10 last:pb-40 last:lg:pb-52",
         props.className
       )}
     >
