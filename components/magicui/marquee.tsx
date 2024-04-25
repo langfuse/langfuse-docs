2b5a404ca1e973b15d77fa41de760fe88f4f1214
Marc Klingen <git@marcklingen.com>
2024-04-25 16:37:37 +0200

    fix: speed of users marquee

--- components/magicui/marquee.tsx
+++ components/magicui/marquee.tsx
@@ -23,7 +23,7 @@ export default function Marquee({
     <div
       {...props}
       className={cn(
-        "group flex overflow-hidden p-2 [--duration:40s] [--gap:1rem] [gap:var(--gap)]",
+        "group flex overflow-hidden p-2 [--duration:30s] [--gap:1rem] [gap:var(--gap)]",
         {
           "flex-row": !vertical,
           "flex-col": vertical,
