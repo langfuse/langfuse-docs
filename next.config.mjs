4f3a43c81a56c6e3f0e9cebe696539393f8afb42
Marc Klingen <git@marcklingen.com>
2024-05-22 00:16:34 +0200

    docs: add mirascope cookbook

--- next.config.mjs
+++ next.config.mjs
@@ -159,6 +159,7 @@ const nonPermanentRedirects = [
   ["/docs/integrations/openai/track-errors", "/docs/integrations/openai/python/track-errors"],
   ["/docs/integrations/openai/python", "/docs/integrations/openai/python/get-started"],
   ["/docs/integrations/openai/js", "/docs/integrations/openai/js/get-started"],
+  ["/docs/integrations/mirascope", "/docs/integrations/mirascope/tracing"],
   ["/docs/flowise", "/docs/integrations/flowise"],
   ["/docs/litellm", "/docs/integrations/litellm/tracing"],
   ["/docs/integrations/litellm", "/docs/integrations/litellm/tracing"],
