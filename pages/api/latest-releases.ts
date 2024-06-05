0cfb4c804c9bdd00d3c655d3362e70e4d3ed7991
Marc Klingen <git@marcklingen.com>
2024-06-06 00:57:05 +0200

    cache in ram to reduce latency and github api requests

--- pages/api/latest-releases.ts
+++ pages/api/latest-releases.ts
@@ -9,6 +9,67 @@ const REPOS = [
 const GITHUB_REPO_API_URL_RELEASE = (repo: string) =>
   `https://api.github.com/repos/${repo}/releases`;
 
+const CACHE_MAX_AGE = 15 * 60 * 1000; // 15 minutes
+
+type ApiResponse = {
+  repo: string;
+  latestRelease?: string;
+  publishedAt?: string;
+  url?: string;
+};
+
+const cache: {
+  date: Date | null;
+  data: ApiResponse[] | null;
+} = {
+  date: null,
+  data: null,
+};
+
+const getLatestReleases = async (): Promise<ApiResponse[]> => {
+  if (
+    cache.date &&
+    cache.data &&
+    Date.now() - cache.date.getTime() < CACHE_MAX_AGE
+  ) {
+    return cache.data;
+  }
+
+  const headers = {
+    Accept: "application/vnd.github.v3+json",
+  };
+
+  if (process.env.GITHUB_ACCESS_TOKEN) {
+    headers["Authorization"] = `token ${process.env.GITHUB_ACCESS_TOKEN}`;
+  }
+
+  const responses = await Promise.all(
+    REPOS.map((repo) => fetch(GITHUB_REPO_API_URL_RELEASE(repo), { headers }))
+  );
+
+  if (responses.some((res) => !res.ok)) {
+    throw new Error(`GitHub API responded with an error`);
+  }
+
+  const langfuseReleases = await Promise.all(
+    responses.map(async (response, index) => {
+      const data = await response.json();
+      const latestRelease = data[0];
+      return {
+        repo: REPOS[index],
+        latestRelease: latestRelease ? latestRelease.tag_name : undefined,
+        publishedAt: latestRelease ? latestRelease.published_at : undefined,
+        url: latestRelease ? latestRelease.html_url : undefined,
+      };
+    })
+  );
+
+  cache.date = new Date();
+  cache.data = langfuseReleases;
+
+  return langfuseReleases;
+};
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
@@ -19,39 +80,7 @@ export default async function handler(
       return res.status(405).json({ message: "Method Not Allowed" });
     }
 
-    // Prepare headers for the GitHub API request
-    const headers = {
-      Accept: "application/vnd.github.v3+json",
-    };
-
-    // If a GitHub access token is provided in the environment variables, include it in the headers
-    if (process.env.GITHUB_ACCESS_TOKEN) {
-      headers["Authorization"] = `token ${process.env.GITHUB_ACCESS_TOKEN}`;
-    }
-
-    // Fetch the repository information from GitHub API
-    const responses = await Promise.all(
-      REPOS.map((repo) => fetch(GITHUB_REPO_API_URL_RELEASE(repo), { headers }))
-    );
-
-    // If the response is not successful, throw an error
-    if (responses.some((res) => !res.ok)) {
-      throw new Error(`GitHub API responded with an error`);
-    }
-
-    // Parse the response data and extract the latest release tag
-    const langfuseReleases = await Promise.all(
-      responses.map(async (response, index) => {
-        const data = await response.json();
-        const latestRelease = data[0]; // Assuming the latest release is the first in the array
-        return {
-          repo: REPOS[index],
-          latestRelease: latestRelease ? latestRelease.tag_name : undefined,
-          publishedAt: latestRelease ? latestRelease.published_at : undefined,
-          url: latestRelease ? latestRelease.html_url : undefined,
-        };
-      })
-    );
+    const langfuseReleases = await getLatestReleases();
 
     return res
       .status(200)
