bd78cb8b2943b32010bab20dccc906a50f32ac13
Marc Klingen <git@marcklingen.com>
2024-06-05 23:45:37 +0200

    chore: update footer

--- components/FooterMenu.tsx
+++ components/FooterMenu.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const menuItems: {
   heading: string;
-  items: { name: string; href: string }[];
+  items: { name: string; href: string; notificationCount?: number }[];
 }[] = [
   {
     heading: "Platform",
@@ -133,7 +133,7 @@ const menuItems: {
     heading: "About",
     items: [
       { name: "Blog", href: "/blog" },
-      { name: "Careers", href: "/careers" },
+      { name: "Careers", href: "/careers", notificationCount: 3 },
       {
         name: "About us",
         href: "/about",
@@ -173,18 +173,23 @@ const FooterMenu = () => {
       <div className="grid grid-cols-2 md:grid-cols-5 text-base gap-y-8 gap-x-2">
         {menuItems.map((menu) => (
           <div key={menu.heading}>
-            <p className="pb-2 font-mono font-bold text-primary">
+            <p className="pb-4 font-mono font-bold text-primary">
               {menu.heading}
             </p>
-            <ul className="flex flex-col gap-2">
+            <ul className="flex flex-col gap-3 items-start">
               {menu.items.map((item) => (
-                <li key={item.name}>
+                <li key={item.name} className="relative flex gap-1">
                   <Link
                     href={item.href}
-                    className="text-sm leading-tight hover:text-primary/80"
+                    className="text-sm hover:text-primary/80"
                   >
                     {item.name}
                   </Link>
+                  {item.notificationCount > 0 && (
+                    <span className="transform -translate-y-1/4 bg-primary text-primary-foreground rounded-full h-3 w-3 text-[0.6rem] flex items-center justify-center">
+                      {item.notificationCount}
+                    </span>
+                  )}
                 </li>
               ))}
             </ul>
