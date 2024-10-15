f8fe0009334ee085a31bbc47ae1108e981ed749a
Hassieb Pakzad <68423100+hassiebp@users.noreply.github.com>
2024-10-14 22:39:58 -0700

    docs(llama): make instrumentation preferred (#863)

--- pages/docs/integrations/llama-index/_meta.tsx
+++ pages/docs/integrations/llama-index/_meta.tsx
@@ -1,6 +1,7 @@
 export default {
   "get-started": "Get Started",
-  "example-python": "Example (Python)",
   "example-python-instrumentation-module":
     "Example Instrumentation Module (Python)",
+  "deprecated-llama-index-callback": "(Deprecated) LlamaIndex Callback",
+  "example-python": "(Deprecated) Example LlamaIndex Callback",
 };
