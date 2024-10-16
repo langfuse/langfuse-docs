33378fcc52f0847a44fd7a958e1c44eca0533a23
Marc Klingen <git@marcklingen.com>
2024-10-16 00:49:57 -0700

    chore: move deployment docs to separate documents

---
+++ pages/docs/deployment/platform-guides/_meta.tsx
@@ -0,0 +1,12 @@
+export default {
+  aws: "AWS",
+  azure: "Azure",
+  "google-cloud-platform": "Google Cloud Platform (GCP)",
+  heroku: "Heroku",
+  railway: "Railway",
+  porter: "Porter.run",
+  kubernetes: {
+    title: "Kubernetes",
+    href: "/docs/deployment/self-host#kubernetes-deployments",
+  },
+};
