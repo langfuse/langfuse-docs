7db9a63d7d3242e4b752a68226a136d95db6c74f
steffen911 <steffen@langfuse.com>
2024-11-13 16:14:06 +0100

    chore: move reasoning to bottom of page and update meta

--- pages/docs/deployment/_meta.tsx
+++ pages/docs/deployment/_meta.tsx
@@ -2,8 +2,6 @@ export default {
   "feature-overview": "Deployment & Features",
   local: "Local (docker compose)",
   "self-host": "Self-host (docker)",
-  v3: {
-    title: "V3 (in development)",
-    display: "hidden",
-  },
+  "self-host-v3": "Self-host (v3 alpha preview)",
+  "migrate-v2-to-v3": "Migrate from v2 to v3",
 };
