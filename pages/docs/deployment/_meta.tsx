72b73f545418fb0e5c5cc360b4419b8bfcb13f9d
steffen911 <steffen@langfuse.com>
2024-11-13 16:54:44 +0100

    chore: docker compose deployment guide

--- pages/docs/deployment/_meta.tsx
+++ pages/docs/deployment/_meta.tsx
@@ -2,6 +2,7 @@ export default {
   "feature-overview": "Deployment & Features",
   local: "Local (docker compose)",
   "self-host": "Self-host (docker)",
-  "self-host-v3": "Self-host (v3 alpha preview)",
+  "self-host-v3": "Self-host (v3 preview)",
   "migrate-v2-to-v3": "Migrate from v2 to v3",
+  guides: "Guides (v3 preview)",
 };
---
+++ pages/docs/deployment/guides/_meta.tsx
@@ -0,0 +1,3 @@
+export default {
+  "docker-compose": "Docker Compose",
+};
