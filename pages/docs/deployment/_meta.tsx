45ae85f508abc97f2f3f4bfe04a024e1ee73dfe2
steffen911 <steffen@langfuse.com>
2024-11-14 16:40:39 +0100

    chore: cleanup

--- pages/docs/deployment/_meta.tsx
+++ pages/docs/deployment/_meta.tsx
@@ -2,7 +2,5 @@ export default {
   "feature-overview": "Deployment & Features",
   local: "Local (docker compose)",
   "self-host": "Self-host (docker)",
-  "self-host-v3": "Self-host (v3 preview)",
-  "migrate-v2-to-v3": "Migrate from v2 to v3",
-  guides: "Guides (v3 preview)",
+  v3: "V3 (Preview)",
 };
