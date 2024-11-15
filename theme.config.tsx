915ef03429561acc0b93ba605def2b611b5af108
Jannik Maierhöfer <jannik.maierhoefer@gmail.com>
2024-11-15 10:33:16 +0100

    docs: add launch week day 0 banner

--- theme.config.tsx
+++ theme.config.tsx
@@ -190,17 +190,17 @@ const config: DocsThemeConfig = {
     Video,
   },
   banner: {
-    key: "thoughtworks-radar",
+    key: "launch-week-2-day-0",
     dismissible: true,
     content: (
-      <Link href="https://www.thoughtworks.com/en-de/radar/platforms/summary/langfuse">
+      <Link href="/launch">
         {/* mobile */}
         <span className="sm:hidden">
-          Langfuse on Thoughtworks Technology Radar →
+          Launch Week #2 – Vercel AI SDK →
         </span>
         {/* desktop */}
         <span className="hidden sm:inline">
-          Langfuse featured on Thoughtworks Technology Radar →
+        Launch Week #2 – Vercel AI SDK →
         </span>
       </Link>
     ),
