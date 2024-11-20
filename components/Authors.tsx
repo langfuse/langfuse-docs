48cffbbb9597db409a67b07a21fc509b7d7a4f93
Marc Klingen <git@marcklingen.com>
2024-11-20 23:07:46 +0100

    feat: expand authors

--- components/Authors.tsx
+++ components/Authors.tsx
@@ -1,3 +1,4 @@
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 
 export const allAuthors = {
@@ -25,44 +26,55 @@ export const allAuthors = {
     image: "/images/people/hassiebpakzad.jpg",
     twitter: "hassiebpakzad",
   },
-  richardkruemmel: {
-    firstName: "Richard",
-    name: "Richard Krümmel",
-    image: "/images/people/richardkruemmel.jpg",
-    twitter: "RichardKrue",
-  },
   marliesmayerhofer: {
     firstName: "Marlies",
     name: "Marlies Mayerhofer",
     image: "/images/people/marliesmayerhofer.jpg",
     twitter: "marliessophie",
   },
+  jannikmaierhoefer: {
+    firstName: "Jannik",
+    name: "Jannik Maierhöfer",
+    image: "/images/people/jannikmaierhoefer.jpg",
+    twitter: "jmaierhoefer",
+  },
+  steffenschmitz: {
+    firstName: "Steffen",
+    name: "Steffen Schmitz",
+    image: "/images/people/steffenschmitz.jpg",
+  },
+
   lydiayou: {
     firstName: "Lydia",
     name: "Lydia You",
     image: "/images/people/lydiayou.jpg",
   },
-  jannikmaierhoefer: {
-    firstName: "Jannik",
-    name: "Jannik Maierhöfer",
-    image: "/images/people/jannikmaierhoefer.jpg",
+  richardkruemmel: {
+    firstName: "Richard",
+    name: "Richard Krümmel",
+    image: "/images/people/richardkruemmel.jpg",
+    twitter: "RichardKrue",
   },
 } as const;
 
-export const Authors = (props: { authors: (keyof typeof allAuthors)[] }) => {
-  const authors = props.authors.filter((author) => author in allAuthors);
+export const Authors = (props: { authors?: (keyof typeof allAuthors)[] }) => {
+  const authors = props.authors?.filter((author) => author in allAuthors) ?? [];
 
   if (authors.length === 0) return null;
   return (
-    <div className="flex flex-wrap gap-5 sm:gap-10 justify-center py-7">
+    <div className="flex flex-wrap gap-x-10 gap-y-6 justify-center py-7 max-w-xl mx-auto">
       {authors.map((author) => (
-        <Author author={author} key={author} />
+        <Author
+          author={author}
+          key={author}
+          hideLastName={authors.length > 3}
+        />
       ))}
     </div>
   );
 };
 
-export const Author = (props: { author: string }) => {
+export const Author = (props: { author: string; hideLastName?: boolean }) => {
   const author =
     allAuthors[props.author] ??
     Object.values(allAuthors).find(
@@ -87,8 +99,12 @@ export const Author = (props: { author: string }) => {
           className="rounded-full"
           alt={`Picture ${author.name}`}
         />
-        <span className="text-primary/60 group-hover:text-primary whitespace-nowrap">
-          {author.name}
+        <span
+          className={cn(
+            "text-primary/60 group-hover:text-primary whitespace-nowrap"
+          )}
+        >
+          {props.hideLastName ? author.firstName : author.name}
         </span>
       </div>
     </a>
