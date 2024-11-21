dbe2ff6604a662506f7601392fc7d33947b9ad7d
Steffen Schmitz <steffen@langfuse.com>
2024-11-21 12:04:43 +0100

    feat: add troubleshooting guide outline (#1002)

--- pages/docs/deployment/v3/_meta.tsx
+++ pages/docs/deployment/v3/_meta.tsx
@@ -1,6 +1,7 @@
 export default {
   overview: "Overview",
   "migrate-v2-to-v3": "Migrate from v2 to v3",
+  troubleshooting: "Troubleshooting",
   guides: "Guides",
   components: "Components",
 };
