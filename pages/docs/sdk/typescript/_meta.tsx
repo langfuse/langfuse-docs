74094fecd65c80e004e5f2066875a8430382cc59
Marc Klingen <git@marcklingen.com>
2024-11-28 15:17:48 +0100

    chore: remove old vercel ai sdk example notebook as it is outdated

--- pages/docs/sdk/typescript/_meta.tsx
+++ pages/docs/sdk/typescript/_meta.tsx
@@ -1,7 +1,6 @@
 export default {
   guide: "Guide",
   "guide-web": "Guide (Web)",
-  "example-vercel-ai": "Example (Vercel AI)",
   reference: {
     title: "Reference ↗",
     href: "https://js.reference.langfuse.com",
