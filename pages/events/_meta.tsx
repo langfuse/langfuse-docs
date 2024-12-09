96cb4cd0971f83049a0616cca889a88b7b4577ab
Marc Klingen <git@marcklingen.com>
2024-12-09 21:58:48 +0100

    chore: change event typesetting and add noindex flag

--- pages/events/_meta.tsx
+++ pages/events/_meta.tsx
@@ -1,7 +1,9 @@
 export default {
-  hacknight2024: {
-    title: "AI Hack Night 2024",
+  "*": {
     type: "page",
     display: "hidden",
+    theme: {
+      typesetting: "article",
+    },
   },
 };
