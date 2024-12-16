31d289b0cfc01b8e0f56b0e94c6b1aca6b9e24e7
Marc Klingen <git@marcklingen.com>
2024-12-16 22:03:55 +0100

    feat: add dependency tracking for js sdk (#1106)

--- scripts/update_package_usage.sh
+++ scripts/update_package_usage.sh
@@ -1,2 +1,5 @@
 github-dependents-info --repo langfuse/langfuse-python --markdownfile ./components-mdx/dependents/python.md --sort stars --mergepackages --verbose --minstars 10
 sed -i '' 's/# Dependents/### Dependents/' ./components-mdx/dependents/python.md
+
+github-dependents-info --repo langfuse/langfuse-js --markdownfile ./components-mdx/dependents/js.md --sort stars --mergepackages --verbose --minstars 10
+sed -i '' 's/# Dependents/### Dependents/' ./components-mdx/dependents/js.md
