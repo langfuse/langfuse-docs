3964925d7422ca4dca47043c70af699af210b75a
Marc Klingen <git@marcklingen.com>
2025-02-06 12:06:19 +0100

    docs: add otel example using the python sdk

--- pages/docs/opentelemetry/_meta.tsx
+++ pages/docs/opentelemetry/_meta.tsx
@@ -1,5 +1,6 @@
 export default {
   "get-started": "Overview",
+  "example-python-sdk": "Example: OTel Python SDK",
   "example-openlit": "Example: OpenLIT",
   "example-openllmetry": "Example: OpenLLMetry",
 };
