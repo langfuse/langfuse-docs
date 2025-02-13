8cb2fc9ee9fd0459342a6cbe4204c275b9cdc54c
Jannik Maierhöfer <48529566+jannikmaierhoefer@users.noreply.github.com>
2025-02-13 17:25:23 +0100

    docs: ad llamaindex workflows integration (#1251)
    * docs: ad llamaindex workflows integration
    
    * update guide

--- pages/docs/integrations/llama-index/_meta.tsx
+++ pages/docs/integrations/llama-index/_meta.tsx
@@ -1,8 +1,9 @@
 export default {
   "get-started": "Get Started",
   "example-python-instrumentation-module":
-    "Example Instrumentation Module (Python)",
+   "Example Instrumentation Module (Python)",
+  "workflows": "LlamaIndex Workflows",
   "example-js-llamaindex": "Example OpenTelemetry (JS/TS)",
   "deprecated-llama-index-callback": "(Deprecated) LlamaIndex Callback",
-  "example-python": "(Deprecated) Example LlamaIndex Callback",
+  "example-python": "(Deprecated) Example LlamaIndex Callback"
 };
