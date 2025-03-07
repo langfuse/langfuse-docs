210a37d6996b88a3adff6698aa01dfcabfdeab9b
Marc Klingen <git@marcklingen.com>
2025-03-07 01:16:17 +0100

    chore: add index pages to videos and cookbook

--- components/VideoIndex.tsx
+++ components/VideoIndex.tsx
@@ -8,40 +8,42 @@ export const VideoIndex = () => (
   <Cards num={3}>
     {(
       getPagesUnderRoute("/guides/videos") as Array<Page & { frontMatter: any }>
-    ).map((page, i) => (
-      <Cards.Card
-        href={page.route}
-        key={page.route}
-        title={
-          page.frontMatter?.title ||
-          page.name
-            .split("_")
-            .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-            .join(" ")
-        }
-        icon={<Video />}
-        arrow
-      >
-        {page.frontMatter.ogImage ? (
-          <div className="relative aspect-video">
-            <Image
-              src={page.frontMatter.ogImage}
-              alt={
-                page.frontMatter?.title ||
-                page.name
-                  .split("_")
-                  .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-                  .join(" ")
-              }
-              objectFit="cover"
-              fill
-              sizes="(max-width: 560px) 100vw, (max-width: 1350px) 50vw, 33vw"
-            />
-          </div>
-        ) : (
-          ""
-        )}
-      </Cards.Card>
-    ))}
+    )
+      .filter((page) => page.route !== "/guides/videos")
+      .map((page, i) => (
+        <Cards.Card
+          href={page.route}
+          key={page.route}
+          title={
+            page.frontMatter?.title ||
+            page.name
+              .split("_")
+              .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+              .join(" ")
+          }
+          icon={<Video />}
+          arrow
+        >
+          {page.frontMatter.ogImage ? (
+            <div className="relative aspect-video">
+              <Image
+                src={page.frontMatter.ogImage}
+                alt={
+                  page.frontMatter?.title ||
+                  page.name
+                    .split("_")
+                    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+                    .join(" ")
+                }
+                objectFit="cover"
+                fill
+                sizes="(max-width: 560px) 100vw, (max-width: 1350px) 50vw, 33vw"
+              />
+            </div>
+          ) : (
+            ""
+          )}
+        </Cards.Card>
+      ))}
   </Cards>
 );
