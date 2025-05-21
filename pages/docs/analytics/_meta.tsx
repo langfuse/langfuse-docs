4bcfa681c58658dd6081fb600b467c7cf8b1096f
Steffen Schmitz <steffen@langfuse.com>
2025-05-21 12:29:17 +0200

    chore: update rate-limits for metrics-api, deprecate daily metrics (#1566)

--- pages/docs/analytics/_meta.tsx
+++ pages/docs/analytics/_meta.tsx
@@ -2,7 +2,6 @@ export default {
   overview: "Overview",
   "custom-dashboards": "Custom Dashboards",
   "metrics-api": "Metrics API",
-  "daily-metrics-api": "Daily Metrics API (legacy)",
   "example-intent-classification": "Intent Classification (Example)",
   integrations: "Integrations",
 };
