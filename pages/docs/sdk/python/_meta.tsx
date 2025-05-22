84366fa4fb9a11c459a6a7b70bcec0ff8c637df8
Hassieb Pakzad <68423100+hassiebp@users.noreply.github.com>
2025-05-22 18:18:39 +0200

    push

--- pages/docs/sdk/python/_meta.tsx
+++ pages/docs/sdk/python/_meta.tsx
@@ -1,7 +1,8 @@
 export default {
-  decorators: "Decorators",
-  example: "Example Notebook",
-  "low-level-sdk": "Low-level SDK",
+  "sdk-v3": "OTEL-based Python SDK (v3)",
+  decorators: "Decorators (v2)",
+  example: "Example Notebook (v2)",
+  "low-level-sdk": "Low-level SDK (v2)",
   reference: {
     title: "Reference ↗",
     href: "https://python.reference.langfuse.com",
