7ff8c95c9fd8aed1e2564c6080722378e9f90382
Hassieb Pakzad <68423100+hassiebp@users.noreply.github.com>
2025-06-02 17:02:47 +0200

    push

--- pages/docs/sdk/python/_meta.tsx
+++ pages/docs/sdk/python/_meta.tsx
@@ -1,6 +1,6 @@
 export default {
   decorators: "Decorators (v2)",
-  "sdk-v3": "OTEL-based Python SDK (v3-beta)",
+  "sdk-v3": "OTEL-based Python SDK (v3)",
   example: "Example Notebook (v2)",
   "low-level-sdk": "Low-level SDK (v2)",
   reference: {
