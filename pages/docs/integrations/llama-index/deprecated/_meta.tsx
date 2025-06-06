26a3aa9e935a0325b463e3fa585772fd20c23642
Jannik Maierhöfer <48529566+jannikmaierhoefer@users.noreply.github.com>
2025-06-06 12:31:55 +0200

    docs: upgrade llamaindex & semantic kernel to SDK v3 (#1602)
    * docs: upgrade integration pages to python SDK v3
    
    * docs: update semantic kernel
    
    * push
    
    * semantic kernel update
    
    * semantic kernel update
    
    * push
    
    * semantic kernel
    
    * push
    
    * push
    
    * push
    
    * push
    
    * push
    
    * push
    
    * links update

---
+++ pages/docs/integrations/llama-index/deprecated/_meta.tsx
@@ -0,0 +1,7 @@
+export default {
+    "get-started": "(Deprecated) Get Started",
+    "example-python-instrumentation-module":
+     "(Deprecated) Instrumentation Module",
+    "deprecated-llama-index-callback": "(Deprecated) Callback",
+    "example-python": "(Deprecated) Example Callback"
+  };
