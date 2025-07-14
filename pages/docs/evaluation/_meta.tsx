e5e7c2c899cf6d2a6e9a725263789f4007937b2c
Marc Klingen <git@marcklingen.com>
2025-07-14 16:52:43 +0200

    push wip

--- pages/docs/evaluation/_meta.tsx
+++ pages/docs/evaluation/_meta.tsx
@@ -1,9 +1,6 @@
 export default {
   overview: "Overview",
   "data-model": "Data Model",
-  annotation: "Human Annotation",
-  "model-based-evals": "LLM-as-a-judge",
-  custom: "Custom via SDKs/API",
-  "user-feedback": "SDKs/API: User Feedback",
-  "external-evaluation-pipelines": "SDKs/API: Evaluation Pipelines",
+  "get-started": "Get Started",
+  features: "Features",
 };
