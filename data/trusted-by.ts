10d56fa760e4d2f68823724677163666cfd90712
Felix Krauth <57024447+felixkrrr@users.noreply.github.com>
2025-07-16 09:34:52 +0200

    changed order of logos

--- data/trusted-by.ts
+++ data/trusted-by.ts
@@ -15,21 +15,21 @@ export const trustedByData: TrustedByData = {
       // No logos assigned to Hobby plan yet
     ],
     "Core": [
-      { name: "Knowunity", logo: "/images/customers/picing-logos/knowunity.png" },
-      { name: "Draftbit", logo: "/images/customers/picing-logos/draftbit.png" },
+      //{ name: "Knowunity", logo: "/images/customers/picing-logos/knowunity.png" },
       { name: "Kombo", logo: "/images/customers/picing-logos/kombo.png" },
+      { name: "Draftbit", logo: "/images/customers/picing-logos/draftbit.png" },
       //{ name: "GoDaddy", logo: "/images/customers/picing-logos/godaddy.png" },
     ],
     "Pro": [
       { name: "Twilio", logo: "/images/customers/picing-logos/twilio.png" },
-      { name: "Khan Academy", logo: "/images/customers/picing-logos/khanacedemy.png" },
       { name: "SumUp", logo: "/images/customers/picing-logos/sumup.png" },
       //{ name: "Brevo", logo: "/images/customers/picing-logos/brevo.png" },
       
     ],
     "Enterprise": [
-      { name: "Cinc Systems", logo: "/images/customers/picing-logos/cincsystems.png" },
-      { name: "HG Capital", logo: "/images/customers/picing-logos/hgcapital.png" },
+      { name: "Khan Academy", logo: "/images/customers/picing-logos/khanacedemy.png" },
+      //{ name: "Cinc Systems", logo: "/images/customers/picing-logos/cincsystems.png" },
+      //{ name: "HG Capital", logo: "/images/customers/picing-logos/hgcapital.png" },
       //{ name: "Dixa", logo: "/images/customers/picing-logos/dixa.png" },
     ],
   },
