0bc8ba9eca093c56febd3e49cd62b90e7f79be43
Marc Klingen <git@marcklingen.com>
2025-07-16 14:44:38 +0200

    test

---
+++ pages/docs/prompt-management/features/_meta.tsx
@@ -0,0 +1,15 @@
+export default {
+  "-- Core": {
+    type: "separator",
+    title: "Core",
+  },
+  caching: "Caching",
+
+  "-- Other": {
+    type: "separator",
+    title: "Other",
+  },
+  "*": {
+    layout: "default",
+  },
+};
