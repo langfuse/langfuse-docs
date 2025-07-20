60ef3505c10eb7f691cd0d14d6e1381b6bdc4902
Marc Klingen <git@marcklingen.com>
2025-07-20 15:21:57 -0700

    evals

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -32,7 +32,6 @@ export default function App({ Component, pageProps }) {
       hsPageView(path);
     };
     router.events.on("routeChangeComplete", handleRouteChange);
-
     return () => {
       router.events.off("routeChangeComplete", handleRouteChange);
     };
