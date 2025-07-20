5f3462d1862ea0053a14c3b303e0c61e42414557
Marc Klingen <git@marcklingen.com>
2025-07-19 18:54:17 -0700

    ui

--- components/MenuSubSeparator.tsx
+++ components/MenuSubSeparator.tsx
@@ -1,7 +1,7 @@
 export function MenuSubSeparator({ children }: { children: React.ReactNode }) {
   return (
     // Subseparator css class used to remove styling from the nextra menu separator (parent) via overrides.css
-    <span className="subseparator _text-gray-500 dark:_text-neutral-400 font-medium text-xs uppercase  border-b border-l border-muted bg-muted-background rounded-bl-md inline-block px-2 py-1 mb-1 w-full tracking-tight">
+    <span className="subseparator _text-gray-500 dark:_text-neutral-400 font-semibold text-xs uppercase  border-b border-l border-muted bg-muted-background rounded-bl-md inline-block px-2 py-1 mb-1 w-full">
       {children}
     </span>
   );
