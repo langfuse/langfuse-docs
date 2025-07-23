d6995b0881135b0e0bbab229427527f3a675b05d
Jannik Maierhöfer <jannik@langfuse.com>
2025-07-23 17:46:27 +0200

    update

--- pages/docs/evaluation/features/_meta.tsx
+++ pages/docs/evaluation/features/_meta.tsx
@@ -1,20 +1,6 @@
-import { MenuSubSeparator } from "@/components/MenuSubSeparator";
-
 export default {
-  "-- Core": {
-    type: "separator",
-    title: <MenuSubSeparator>Core</MenuSubSeparator>,
-  },
   "llm-as-a-judge": "LLM as a Judge",
   "custom-scores": "Custom Evaluators",
   "annotation": "Manual Annotation",
-
-  "-- Advanced": {
-    type: "separator",
-    title: <MenuSubSeparator>Advanced</MenuSubSeparator>,
-  },
-  "security-and-guardrails": {},
-  "*": {
-    layout: "default",
-  },
+  "security-and-guardrails": "Security & Guardrails",
 };
