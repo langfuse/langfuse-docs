c5432acbb1a269465a5cf2161e4479d4b2088ff4
Felix Krauth <57024447+felixkrrr@users.noreply.github.com>
2025-07-31 18:52:04 +0200

    change to native and remote dataset Runs

--- pages/docs/evaluation/dataset-runs/_meta.tsx
+++ pages/docs/evaluation/dataset-runs/_meta.tsx
@@ -1,6 +1,6 @@
 export default {
   "data-model": "Data Model",
   datasets: "Datasets",
-  "run-via-sdk": "Run via SDK",
-  "run-via-ui": "Run via UI",
+  "native-run": "Native Run",
+  "remote-run": "Remote Run",
 };
