737f3677194e50d2abf1ec1599dca7cf55536ca4
Hassieb Pakzad <68423100+hassiebp@users.noreply.github.com>
2025-08-15 16:54:42 +0200

    fix otel setup

--- src/instrumentation.node.ts
+++
@@ -1,8 +0,0 @@
-import { NodeSDK } from "@opentelemetry/sdk-node";
-import { LangfuseSpanProcessor } from "@langfuse/otel";
-
-const sdk = new NodeSDK({
-  serviceName: "langfuse-docs",
-  spanProcessors: [new LangfuseSpanProcessor()],
-});
-sdk.start();
