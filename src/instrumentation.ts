20f15136570975fc19add05e8e401d3e14bac980
Hassieb Pakzad <68423100+hassiebp@users.noreply.github.com>
2025-08-15 16:56:37 +0200

    push

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -2,9 +2,9 @@ import { NodeTracerProvider } from "@opentelemetry/sdk-trace-node";
 import { LangfuseSpanProcessor } from "@langfuse/otel";
 
 export function register() {
-const traceProvider = new NodeTracerProvider({
-  spanProcessors: [new LangfuseSpanProcessor()]
-})
+  const traceProvider = new NodeTracerProvider({
+    spanProcessors: [new LangfuseSpanProcessor()],
+  });
 
-traceProvider.register()
+  traceProvider.register();
 }
