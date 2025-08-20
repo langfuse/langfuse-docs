c7832412b0066683cc6aa57e94ada6e672ff2c5a
Hassieb Pakzad <68423100+hassiebp@users.noreply.github.com>
2025-08-20 19:46:12 +0200

    refactor

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,5 +1,30 @@
-export async function register() {
-  if (process.env.NEXT_RUNTIME === "nodejs") {
-    await import("./instrumentation.node.ts");
-  }
+import { LangfuseSpanProcessor, ShouldExportSpan } from "@langfuse/otel";
+import { NodeTracerProvider } from "@opentelemetry/sdk-trace-node";
+
+const shouldExportSpan: ShouldExportSpan = (span) => {
+  return span.otelSpan.instrumentationScope.name !== "next.js";
+};
+
+const euSpanProcessor = new LangfuseSpanProcessor({
+  publicKey: process.env.NEXT_PUBLIC_LANGFUSE_PUBLIC_KEY,
+  shouldExportSpan,
+  exportMode: "immediate",
+});
+
+const usSpanProcessor = new LangfuseSpanProcessor({
+  baseUrl: process.env.NEXT_PUBLIC_US_LANGFUSE_BASE_URL,
+  publicKey: process.env.NEXT_PUBLIC_US_LANGFUSE_PUBLIC_KEY,
+  secretKey: process.env.US_LANGFUSE_SECRET_KEY,
+  shouldExportSpan,
+  exportMode: "immediate",
+});
+
+const spanProcessors = [euSpanProcessor, usSpanProcessor];
+
+export function register() {
+  const tracerProvider = new NodeTracerProvider({
+    spanProcessors,
+  });
+
+  tracerProvider.register();
 }
