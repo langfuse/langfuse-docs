8751d63001388e23a5d68bd25b13e30ef9f7bc96
Jannik Maierhöfer <jannik@langfuse.com>
2025-08-20 13:25:54 +0200

    remove v2 docs

--- pages/docs/observability/sdk/python/_meta.tsx
+++ pages/docs/observability/sdk/python/_meta.tsx
@@ -1,8 +1,5 @@
 export default {
   "sdk-v3": "OTEL-based SDK (v3)",
-  decorators: "Decorators (v2)",
-  example: "Example Notebook (v2)",
-  "low-level-sdk": "Low-level SDK (v2)",
   reference: {
     title: "Reference ↗",
     href: "https://python.reference.langfuse.com",
