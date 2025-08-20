68016a242f1331fdaf6391c93456a7652015946e
Hassieb Pakzad <68423100+hassiebp@users.noreply.github.com>
2025-08-20 16:40:25 +0200

    add wait

--- components/qaChatbot/apiHandler.ts
+++ components/qaChatbot/apiHandler.ts
@@ -97,7 +97,12 @@ export const POST = async (req: Request) => {
         experimental_telemetry: { isEnabled: true },
       });
 
-      after(tracerProvider.forceFlush());
+      after(async () => {
+        await new Promise((resolve) => {
+          setTimeout(resolve, 1_000);
+        });
+        await tracerProvider.forceFlush();
+      });
 
       return result.toUIMessageStreamResponse({
         generateMessageId: () => getActiveTraceId(),
