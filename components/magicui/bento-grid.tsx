1b8054f08217b65cf7da497a85808e39ac623883
Marc Klingen <git@marcklingen.com>
2025-08-30 00:33:17 +0200

    add demo links to bento

--- components/magicui/bento-grid.tsx
+++ components/magicui/bento-grid.tsx
@@ -31,6 +31,7 @@ const BentoCard = ({
   description,
   href,
   cta,
+  demoHref,
 }: {
   name: string;
   className: string;
@@ -39,8 +40,9 @@ const BentoCard = ({
   description: string;
   href: string;
   cta: string;
+  demoHref?: string;
 }) => (
-  <Link
+  <div
     key={name}
     className={cn(
       "group relative col-span-3 flex flex-col justify-between overflow-hidden rounded border",
@@ -50,7 +52,6 @@ const BentoCard = ({
       "transform-gpu dark:bg-transparent dark:backdrop-blur-md",
       className
     )}
-    href={href}
   >
     {background}
     <div />
@@ -64,14 +65,29 @@ const BentoCard = ({
       </p>
     </div>
 
-    <div className="pointer-events-none absolute bottom-0 flex w-full translate-y-10 transform-gpu flex-row items-center p-4 opacity-0 transition-all duration-300 group-hover:translate-y-0 group-hover:opacity-100">
-      <Button variant="ghost" size="sm" className="ml-2 pointer-events-auto">
-        {cta}
-        <ArrowRight className="ml-2 h-4 w-4" />
+    <div className="pointer-events-none absolute bottom-0 flex w-full translate-y-10 transform-gpu flex-row items-center p-4 opacity-0 transition-all duration-300 group-hover:translate-y-0 group-hover:opacity-100 gap-2">
+      <Button variant="ghost" size="sm" className="pointer-events-auto" asChild>
+        <Link href={href}>
+          {cta}
+          <ArrowRight className="ml-2 h-4 w-4" />
+        </Link>
       </Button>
+      {demoHref && (
+        <Button
+          variant="ghost"
+          size="sm"
+          className="pointer-events-auto"
+          asChild
+        >
+          <Link href={demoHref}>
+            See demo
+            <ArrowRight className="ml-2 h-4 w-4" />
+          </Link>
+        </Button>
+      )}
     </div>
     <div className="pointer-events-none absolute inset-0 transform-gpu transition-all duration-300 group-hover:bg-black/[.03] group-hover:dark:bg-neutral-800/20" />
-  </Link>
+  </div>
 );
 
 export { BentoCard, BentoGrid };
