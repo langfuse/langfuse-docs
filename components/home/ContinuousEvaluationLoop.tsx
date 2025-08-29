d734ff38af07c65d29e94db92037b7d1bf4185be
Marc Klingen <git@marcklingen.com>
2025-08-30 01:21:34 +0200

    push

---
+++ components/home/ContinuousEvaluationLoop.tsx
@@ -0,0 +1,15 @@
+import { HomeSection } from "./components/HomeSection";
+import { Header } from "../Header";
+
+export default function ContinuousEvaluationLoop() {
+  return (
+    <HomeSection className="flex flex-col items-center">
+      <Header
+        title="Continuous Evaluation Loop"
+        description={<span>// TODO: Add description</span>}
+        className="mb-0"
+      />
+      {/* TODO: Add content */}
+    </HomeSection>
+  );
+}
