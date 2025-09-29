103d06d23ef784789e71a1660efab8cbfec9783d
Marc Klingen <git@marcklingen.com>
2025-09-29 13:10:20 +0200

    remove unused import

--- components/home/FeatureTabsSection.tsx
+++ components/home/FeatureTabsSection.tsx
@@ -1,11 +1,10 @@
 import { HomeSection } from "./components/HomeSection";
-import { Header } from "../Header";
 import { FeatureTabs, featureTabsData } from "./feature-tabs";
 
 export function FeatureTabsSection() {
   return (
     <HomeSection id="features" className="pt-8 lg:pt-4 2xl:pt-10">
-        <FeatureTabs features={featureTabsData} />
+      <FeatureTabs features={featureTabsData} />
     </HomeSection>
   );
 }
