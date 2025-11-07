577c2788c848e09bceed78be2a01db38b22c7ab4
Michael Fröhlich <15179255+FroeMic@users.noreply.github.com>
2025-11-07 19:05:45 +0100

    docs: LW4-D5 (#2272)
    * Fix: Replace comment and reaction image
    
    * Update comment docs page: shorten title, update api routes
    
    * Add score analytics docs and changelog
    
    * edit LW blog page
    
    * push
    
    * push
    
    ---------
    
    Co-authored-by: Jannik Maierhöfer <jannik@langfuse.com>

--- pages/docs/evaluation/evaluation-methods/_meta.tsx
+++ pages/docs/evaluation/evaluation-methods/_meta.tsx
@@ -2,5 +2,6 @@ export default {
   "llm-as-a-judge": "LLM-as-a-Judge",
   annotation: "Human Annotations",
   "custom-scores": "Custom Scores",
+  "score-analytics": "Score Analytics",
   "data-model": "Data Model",
 };
