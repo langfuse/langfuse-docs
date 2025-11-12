cbcf37123968301c6202a366366cb71b66a189a2
abdallah <a@abedraba.com>
2025-11-12 14:47:37 +0100

    feat: add eval blog and docs section

--- pages/docs/evaluation/_meta.tsx
+++ pages/docs/evaluation/_meta.tsx
@@ -1,5 +1,6 @@
 export default {
   overview: "Overview",
+  tutorials: "Tutorials",
   "evaluation-methods": "Evaluation Methods",
   experiments: "Experiments",
   "troubleshooting-and-faq": "Troubleshooting & FAQ",
