ab8736f7c2d33b2242994b9881d2370d52e9c070
Max Deichmann <m.deichmann@tum.de>
2025-11-25 22:42:21 +0100

    chore: fix engineering handbook (#2342)
    * chore: fix engineering handbook
    
    * chore: fix engineering handbook
    
    * chore: fix engineering handbook

---
+++ pages/handbook/how-we-hire/_meta.tsx
@@ -0,0 +1,9 @@
+export default {
+    "philosophy": "Philosophy",
+    "hiring-process": "Hiring Process",
+    "engineering-super-day": "Engineering Super Day",
+    "*": {
+      layout: "default",
+    },
+  };
+  
