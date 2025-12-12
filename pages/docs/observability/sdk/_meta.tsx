a730337f97bfced09ef1c843691030659f982a15
Jannik Maierhöfer <jannik@langfuse.com>
2025-12-12 11:36:32 +0100

    push

--- pages/docs/observability/sdk/_meta.tsx
+++ pages/docs/observability/sdk/_meta.tsx
@@ -4,4 +4,12 @@ export default {
   "advanced-features": "Advanced Features",
   "troubleshooting-and-faq": "Troubleshooting & FAQ",
   "upgrade-path": "Upgrade Path",
+  python: {
+    title: "Python Reference ↗",
+    href: "https://python.reference.langfuse.com/",
+  },
+  typescript: {
+    title: "JS/TS Reference ↗",
+    href: "https://langfuse-js-git-main-langfuse.vercel.app/",
+  },
 };
