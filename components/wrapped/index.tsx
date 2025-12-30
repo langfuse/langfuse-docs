3b6b86dfdcaed662c20721ec6d50e558776d440a
felixkrrr <57024447+felixkrrr@users.noreply.github.com>
2025-12-30 14:54:27 +0100

    Felix/wrapped (#2421)
    * init
    
    * launches section updated
    
    * update OSS contributors
    
    * updates to metrics
    
    * CUSTOMER GRID
    
    * intro animations
    
    * multiple fixes
    
    * add icons to metrics
    
    * many changes
    
    * changes
    
    * add animations
    
    * final touches
    
    * Update components/wrapped/Metrics.tsx
    
    Co-authored-by: ellipsis-dev[bot] <65095814+ellipsis-dev[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: ellipsis-dev[bot] <65095814+ellipsis-dev[bot]@users.noreply.github.com>

---
+++ components/wrapped/index.tsx
@@ -0,0 +1,25 @@
+import { Background } from "../Background";
+import { Hero } from "./Hero";
+import { Intro } from "./Intro";
+import { Metrics } from "./Metrics";
+import { OSS } from "./OSS";
+import { Launches } from "./Launches";
+import { Customers } from "./Customers";
+import { Outro } from "./Outro";
+
+export function Wrapped() {
+  return (
+    <>
+      <main className="relative overflow-hidden w-full">
+        <Hero />
+        <Intro />
+        <Metrics />
+        <OSS />
+        <Launches />
+        <Customers />
+        <Outro />
+      </main>
+      <Background />
+    </>
+  );
+}
