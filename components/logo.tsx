1ae8d3a8844dd05b4226bfbe0c0f1e074682d295
Tom Antas <tom@altalogy.com>
2026-03-03 17:45:28 +0100

    rev: navbar

--- components/logo.tsx
+++ components/logo.tsx
@@ -16,8 +16,17 @@ export function Logo({
   wrapInLink?: boolean;
 }) {
   const [menuOpen, setMenuOpen] = useState(false);
-  const imageBlock = (
-    <div className="flex gap-2 items-center cursor-pointer -mr-4 md:-mr-0">
+  return (
+    <>
+      <div className="flex items-center">
+        <Link
+          href="/"
+          onContextMenu={(e) => {
+            e.preventDefault();
+            setMenuOpen(true);
+          }}
+        >
+          <div className="flex gap-2 items-center cursor-pointer -mr-4 md:-mr-0">
             <Image
               src="/langfuse_logo_white.svg"
               alt="Langfuse Logo"
@@ -33,40 +42,23 @@ export function Logo({
               className="block dark:hidden max-w-28 sm:max-w-none"
             />
             <style jsx>{`
-              div {
-                mask-image: linear-gradient(
-                  60deg,
-                  #bba0ff 25%,
-                  rgba(187, 160, 255, 0.2) 50%,
-                  #bba0ff 75%
-                );
-                mask-size: 400%;
-                mask-position: 0%;
-              }
-              div:hover {
-                mask-position: 100%;
-                transition: mask-position 1s ease, -webkit-mask-position 1s ease;
-              }
-            `}</style>
+            div {
+              mask-image: linear-gradient(
+                60deg,
+                #bba0ff 25%,
+                rgba(187, 160, 255, 0.2) 50%,
+                #bba0ff 75%
+              );
+              mask-size: 400%;
+              mask-position: 0%;
+            }
+            div:hover {
+              mask-position: 100%;
+              transition: mask-position 1s ease, -webkit-mask-position 1s ease;
+            }
+          `}</style>
           </div>
-  );
-
-  return (
-    <>
-      <div className="flex items-center">
-        {wrapInLink ? (
-          <Link
-            href="/"
-            onContextMenu={(e) => {
-              e.preventDefault();
-              setMenuOpen(true);
-            }}
-          >
-            {imageBlock}
-          </Link>
-        ) : (
-          imageBlock
-        )}
+        </Link>
       </div>
       {menuOpen && <ContextMenu open={menuOpen} setOpen={setMenuOpen} />}
     </>
