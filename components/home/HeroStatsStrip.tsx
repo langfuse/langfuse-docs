90a73d458aa25c0c9cb7ffe8f3bca3d737383686
Akio Nuernberger <akio@langfuse.com>
2026-07-24 16:58:09 -0700

    Update Fortune adoption metrics

--- components/home/HeroStatsStrip.tsx
+++ components/home/HeroStatsStrip.tsx
@@ -12,7 +12,7 @@ function StatItems() {
   return (
     <>
       <Text size="s" className="whitespace-nowrap shrink-0">
-        Used by <b className="text-primary">19</b> of Fortune 50
+        Used by <b className="text-primary">21</b> of Fortune 50
       </Text>
       <Dot />
       <Text size="s" className="whitespace-nowrap shrink-0">
